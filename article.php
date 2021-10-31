a094be33b9075dbe5c453eff5e125952ab5f9424
Linux Memory <root@foxsupportmail.site>
2021-10-31 21:43:05 +0100

    Added article.php

---
+++ article.php
@@ -0,0 +1,49 @@
+<?php
+$db_host = "localhost";
+$db_user = "cmsuser";
+$db_pass = "F&se1o81";
+$db_name = "cms";
+
+$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+
+if (mysqli_connect_error()) {
+echo mysqli_connect_error();
+
+}
+
+$db = "select * from articles where id=1;";
+
+$results = mysqli_query($conn, $db);
+
+if ($results === false){
+echo mysqli_error();
+}
+
+$articles = mysqli_fetch_assoc($results);
+
+var_dump($articles);
+
+?>
+
+
+
+
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<title>First PHP page</title>
+	</head>
+<body>
+	<?php if ($articles === null): ?>
+	<p><?php echo "No articles found"; ?></p>
+	<?php else: ?>
+	
+	<h1>Store types</h1>
+	<ul>
+		<li><h3><?php echo $articles['title']; ?></h3></li>
+		<p><?php echo $articles['content']; ?><p>
+	</ul>
+	<?php endif; ?>
+</body>
+</html>
