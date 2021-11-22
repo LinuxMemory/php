6977052f4bc33cdf9c28893eed154d02950ef637
Linux Memory <root@foxsupportmail.site>
2021-11-22 13:08:31 +0100

    mysqli_stmt_bind_param

--- new-article.php
+++ new-article.php
@@ -2,9 +2,43 @@
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
+
 var_dump($_POST);
 
+$db_host = "localhost";
+$db_user = "cmsuser";
+$db_pass = "F&se1o81";
+$db_name = "cms";
+
+$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+
+$db = "INSERT INTO articles (title, content, published_time)
+	   VALUES (?, ?, ?)";
+	
+$stmt = mysqli_prepare($conn, $db);
+	
+if ($stmt === false) {
+	echo mysqli_error($conn);
+} else {
+	
+	mysqli_stmt_bind_param($stmt, "sss", $_POST['title'], $_POST['content'], $_POST['published_time']);
+	
+	if (mysqli_stmt_execute($stmt)) {
+		$id = mysqli_insert_id($conn);
+	echo "Inserted ID equals: $id";
+	}
+	
+else {
+ echo mysqli_stmt_error($stmt);
+
+
+
+	
 }
+}
+}
+
+
 ?>
 
 
@@ -29,8 +63,8 @@ var_dump($_POST);
 		<p></p>
 		
 		<div>
-			<label for="published_at">Publication date and time</label>
-			<input type="datetime" name="published_at" id="published_at" placeholder="Example: 2018-04-31"><br>
+			<label for="published_time">Publication date and time</label>
+			<input type="datetime" name="published_time" id="published_time" placeholder="Example: 2018-04-31"><br>
 		</div>
 		
 		<p></p>
